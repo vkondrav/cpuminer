323e948ff44382f28f7fc6544b7210101dabe893
Vitaliy Kondratiev <kondrav@mpihost01.(none)>
2014-03-14 13:20:48 -0400

    adding some run files

---
+++ run_mpi07.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while :
+do
+	./autogen.sh
+	./configure CFLAGS="-O3"
+	./minerd  --algo=scrypt --url=stratum+tcp://stratum.rapidhash.net:3333 --userpass=ScarletSickle.mpi07:nvr	
+done
